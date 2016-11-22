988cf435ebbc16c77ee510dc2a8dca59dc9a1f0a
MIKE-COMPUTER <MIKE-COMPUTER@DESKTOP-CI78C1B.home>
2016-11-22 20:32:43 +0100

    Ajout de variable nbBomb

--- pMineSweeperMa/src/editing/Tableau.java
+++ pMineSweeperMa/src/editing/Tableau.java
@@ -7,6 +7,7 @@ public class Tableau {
 	private List<List<Integer>> matrice;
 	private int ligne;
 	private int colonne;
+	private int nbBomb;
 	
 	public Tableau(int difficulty){
 		matrice = new ArrayList<>();
@@ -14,13 +15,16 @@ public class Tableau {
 		if(difficulty == 1){
 			this.ligne= 9;
 			this.colonne = 9;
+			this.nbBomb = 10;
 		}else if(difficulty == 2){
 			this.ligne = 16;
 			this.colonne = 16;
+			this.nbBomb = 40;
 			
 		}else if(difficulty == 3 ){
 			this.ligne = 16;
 			this.colonne = 30;
+			this.nbBomb = 99;
 		}
 		
 		
