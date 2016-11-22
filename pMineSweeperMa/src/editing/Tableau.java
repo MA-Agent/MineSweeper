6e71d00e63cf885362939682b2bf5486cde698c1
Zeyd <Zeyd@Agentz.home>
2016-11-22 19:35:01 +0100

    gen tableau initial

---
+++ pMineSweeperMa/src/editing/Tableau.java
@@ -0,0 +1,13 @@
+package editing;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tableau {
+	private List<List<Integer>> matrice;
+	private int size;
+	public Tableau(int size){
+		matrice = new ArrayList<>();
+	}
+
+}
