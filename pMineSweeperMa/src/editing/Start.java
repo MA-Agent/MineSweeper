30fadb6ca180909c798fdbfaa3bb24293fc285d6
MIKE-COMPUTER <MIKE-COMPUTER@DESKTOP-CI78C1B.home>
2016-11-22 21:10:51 +0100

    Ajout de la classe Start pour lancer le jeu

--- pMineSweeperMa/src/editing/Start.java
+++ pMineSweeperMa/src/editing/Start.java
@@ -1,5 +1,35 @@
 package editing;
 
+import java.util.Scanner;
+
 public class Start {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		int difficulty = scan.nextInt();
+		boolean difCorrect = true;
+		
+		System.out.println("Entrez une difficulté (1/2/3");
+		do
+		{
+		if(difficulty != 1 && difficulty != 2 && difficulty !=3)
+		{
+			System.out.println("Entrez une difficulté (1/2/3)");
+			difCorrect = false;
+		}
+		else
+		{
+			difCorrect = true;
+		}
+		}while(!difCorrect);
+
+
+
+			
+
+		Tableau grille = new Tableau(difficulty);
+		
+	}
 
 }
