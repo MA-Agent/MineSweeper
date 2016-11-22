952a745780451a46208e61dc69130e760d74d2db
agentZed <e>
2016-11-22 20:03:37 +0100

    début constructeur tableau

--- pMineSweeperMa/src/editing/Tableau.java
+++ pMineSweeperMa/src/editing/Tableau.java
@@ -5,9 +5,25 @@ import java.util.List;
 
 public class Tableau {
 	private List<List<Integer>> matrice;
-	private int size;
-	public Tableau(int size){
+	private int ligne;
+	private int colonne;
+	
+	public Tableau(int difficulty){
 		matrice = new ArrayList<>();
+		
+		if(difficulty == 1){
+			this.ligne= 9;
+			this.colonne = 9;
+		}else if(difficulty == 2){
+			this.ligne = 16;
+			this.colonne = 16;
+			
+		}else if(difficulty == 3 ){
+			this.ligne = 16;
+			this.colonne = 30;
+		}
+		
+		
 	}
 
 }
