41b47cc5cf455ff9682218ed29f0232595732b77
pec05 <leandro.peccio@hotmail.com>
2016-11-25 15:16:12 +0100

    Ajout des commentaires

--- pMineSweeperMa/src/editing/FenetreAvecMenu.java
+++ pMineSweeperMa/src/editing/FenetreAvecMenu.java
@@ -6,14 +6,17 @@ import java.awt.event.ActionListener;
 import javax.swing.JFrame;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JTextArea;
 
 public class FenetreAvecMenu extends JFrame
 {
+	//Nom des "menu"
 	private JMenuBar menuBar = new JMenuBar();
 	private JMenuBar menuFichier = new JMenuBar();
 	private JMenuBar menuDifficulté = new JMenuBar();
 	private JMenuBar menuAide = new JMenuBar();
 	
+	//Nom des onglets
 	private JMenuItem item1 = new JMenuItem ("Ouvrir");
 	private JMenuItem item2 = new JMenuItem ("Fermer");
 	private JMenuItem item3 = new JMenuItem ("Facile");
@@ -23,10 +26,10 @@ public class FenetreAvecMenu extends JFrame
 	
 	public FenetreAvecMenu ()
 	{
-		setSize(800,500);
+		setSize(800,500);// taille de la fenêtre
 		setTitle("Démineur");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//Permet de fermer la fenêtre avec la croix
+		setLocationRelativeTo(null);//Centre la fenêtre au millieu
 		
 		menuFichier.add(item1);
 		item2.addActionListener(new ActionListener()
@@ -36,17 +39,18 @@ public class FenetreAvecMenu extends JFrame
 						dispose();
 					}
 				});
+		
 		menuFichier.add(item2);
 		menuDifficulté.add(item3);
 		menuDifficulté.add(item4);
 		menuDifficulté.add(item5);	
 		menuAide.add(item6);
-		
 		menuBar.add(menuFichier);
 		menuBar.add(menuDifficulté);
 		menuBar.add(menuAide);
 		
 		setJMenuBar(menuBar);
+	
 		setVisible(true);
 				
 	}
